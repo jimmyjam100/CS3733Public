43e1496775ad3c967c8d75d4da0f0840f4de510e
unknown <jimmyjam100@aol.com>
2018-11-28 11:01:38 -0500

    added the project folder

---
+++ ScheduleProject/src/main/java/edu/wpi/cs/yidun/db/ScheduleDAO.java
@@ -0,0 +1,50 @@
+package edu.wpi.cs.yidun.db;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.cs.yidun.model.Schedule;
+
+public class ScheduleDAO {
+
+	java.sql.Connection conn;
+
+    public ScheduleDAO() {
+    	try  {
+    		conn = DatabaseUtil.connect();
+    	} catch (Exception e) {
+    		conn = null;
+    	}
+    }
+    
+    
+    public boolean addConstant(Schedule sched) throws Exception {
+        try {
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM Schedules WHERE date = ?;");
+            ps.setDate(1, sched.date);
+            ResultSet resultSet = ps.executeQuery();
+            
+            // already present?
+            while (resultSet.next()) {
+                Schedule c = generateSchedule(resultSet);
+                resultSet.close();
+                return false;
+            }
+
+            ps = conn.prepareStatement("INSERT INTO Schedules (date) values(?);");
+            ps.setDate(1,  sched.date);
+            ps.execute();
+            return true;
+
+        } catch (Exception e) {
+            throw new Exception("Failed to insert constant: " + e.getMessage());
+        }
+    }
+    
+    private Schedule generateSchedule(ResultSet resultSet) throws Exception {
+        Date d  = resultSet.getDate("date");
+        return new Schedule(d);
+    }
+
+}
---
+++ ScheduleProject/src/main/java/edu/wpi/cs/yidun/model/Schedule.java
@@ -0,0 +1,11 @@
+package edu.wpi.cs.yidun.model;
+
+import java.sql.Date;
+
+public class Schedule {
+	public Date date;
+	public Schedule(Date n) {
+		date = n;
+	}
+
+}
