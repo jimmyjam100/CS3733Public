19459b6030cbcd3cdaa45166784466c9ee004cea
unknown <jimmyjam100@aol.com>
2018-12-05 13:26:37 -0500

    made CloseTimeslot

---
+++ ScheduleProject/src/main/java/edu/wpi/cs/yildun/demo/CloseTImeslotLambda.java
@@ -0,0 +1,89 @@
+package edu.wpi.cs.yildun.demo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.google.gson.Gson;
+
+import edu.wpi.cs.yidun.db.ScheduleDAO;
+
+public class CloseTImeslotLambda implements RequestStreamHandler {
+	
+	void closeTimeslot(int id){
+		ScheduleDAO dao = new ScheduleDAO();
+	}
+
+    @Override
+	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+		LambdaLogger logger = context.getLogger();
+		logger.log("Loading Java Lambda handler of RequestStreamHandler");
+
+		JSONObject headerJson = new JSONObject();
+		headerJson.put("Content-Type",  "application/json");  // not sure if needed anymore?
+		headerJson.put("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+	    headerJson.put("Access-Control-Allow-Origin",  "*");
+	        
+		JSONObject responseJson = new JSONObject();
+		responseJson.put("headers", headerJson);
+
+		CloseTimeslotResponse response = null;
+		
+		// extract body from incoming HTTP POST request. If any error, then return 422 error
+		String body;
+		boolean processed = false;
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+			JSONParser parser = new JSONParser();
+			JSONObject event = (JSONObject) parser.parse(reader);
+			logger.log("event:" + event.toJSONString());
+			
+			String method = (String) event.get("httpMethod");
+			if (method != null && method.equalsIgnoreCase("OPTIONS")) {
+				logger.log("Options request");
+				response = new CloseTimeslotResponse(200);  // OPTIONS needs a 200 response
+		        responseJson.put("body", new Gson().toJson(response));
+		        processed = true;
+		        body = null;
+			} else {
+				body = (String)event.get("body");
+				if (body == null) {
+					body = event.toJSONString();  // this is only here to make testing easier
+				}
+			}
+		} catch (ParseException pe) {
+			logger.log(pe.toString());
+			response = new CloseTimeslotResponse(422);  // unable to process input
+	        responseJson.put("body", new Gson().toJson(response));
+	        processed = true;
+	        body = null;
+		}
+
+		if (!processed) {
+			CloseTimeslotRequest req = new Gson().fromJson(body, CloseTimeslotRequest.class);
+			CloseTimeslotResponse resp;
+			closeTimeslot(req.id);
+			resp = new CloseTimeslotResponse(200);
+			// compute proper response
+	        responseJson.put("body", new Gson().toJson(resp));  
+		}
+		
+        logger.log("end result:" + responseJson.toJSONString());
+        logger.log(responseJson.toJSONString());
+        OutputStreamWriter writer = new OutputStreamWriter(output, "UTF-8");
+        writer.write(responseJson.toJSONString());  
+        writer.close();
+	}
+
+
+}
---
+++ ScheduleProject/src/main/java/edu/wpi/cs/yildun/demo/CloseTimeslotRequest.java
@@ -0,0 +1,14 @@
+package edu.wpi.cs.yildun.demo;
+
+public class CloseTimeslotRequest {
+	int id;
+	
+	public CloseTimeslotRequest(int id) {
+		this.id = id;
+	}
+	
+	public String toString() {
+		return "CloseTimeslotRequest(" + id + ")";
+	}
+
+}
