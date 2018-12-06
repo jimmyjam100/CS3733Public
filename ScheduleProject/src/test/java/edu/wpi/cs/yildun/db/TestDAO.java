e0863043e40203b89ff9afa02ec88dbf8b918aa5
Daniel Ribaudo <dannyribaudo@gmail.com>
2018-12-06 09:35:26 -0500

    Update TestDAO

--- ScheduleProject/src/test/java/edu/wpi/cs/yildun/db/TestDAO.java
+++ ScheduleProject/src/test/java/edu/wpi/cs/yildun/db/TestDAO.java
@@ -8,38 +8,57 @@ import org.junit.Before;
 import org.junit.Test;
 
 import edu.wpi.cs.yidun.db.ScheduleDAO;
+import edu.wpi.cs.yidun.model.Day;
 import edu.wpi.cs.yidun.model.Schedule;
+import edu.wpi.cs.yidun.model.Timeslot;
+import edu.wpi.cs.yidun.model.Week;
+import edu.wpi.cs.yildun.demo.LambdaFunctionHandler;
 
 public class TestDAO {
 
+	//TODO !Add more test cases!
+	
 	Schedule schedule;
 	ScheduleDAO dao;
 	
+	@SuppressWarnings("deprecation")
 	@Before
 	public void initAll() {
-		schedule = new Schedule(new Date(), new Date(), 
-				LocalTime.parse("09:00"), LocalTime.parse("16:00"), "Test", "", 60);
+		Date startDate = new Date();
+		Date endDate = new Date();
+		endDate.setDate(startDate.getDate()+1);
+		schedule = LambdaFunctionHandler.newSchedule("Test", startDate, endDate,
+				LocalTime.parse("09:00"), LocalTime.parse("16:00"), 60);
+		
 		dao = new ScheduleDAO();
 	}
 	
 	@Test
-	public void testAddSchedule() {
+	public void testAddAndGetSchedule() {
 		try {
 			dao.addSchedule(schedule);
+			int ts1 = 0, ts2 = 0;
+			for (Week week : schedule.getWeeks()) {
+				for (Day day : week.getDays()) {
+					for (@SuppressWarnings("unused") Timeslot ts : day.getTimeslots()) {
+						ts1++;
+					}
+				}
+			}
+			
+			Schedule schedule2 = dao.getSchedule(schedule.getId());
+			for (Week week : schedule2.getWeeks()) {
+				for (Day day : week.getDays()) {
+					for (@SuppressWarnings("unused") Timeslot ts : day.getTimeslots()) {
+						ts2++;
+					}
+				}
+			}
+			Assert.assertEquals(ts1, ts2);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			Assert.fail();
 		}
 	}
-	
-	@Test
-	public void testGetSchedule() {
-		try {
-			Assert.assertEquals(schedule, dao.getSchedule(schedule.getId()));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
 }
